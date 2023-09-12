5fcc86955cdde1e2d8ff08347bebbfe257a01625
Mr-Samuel-Lutz <mr.samuel.lutz@gmail.com>
2023-09-12 16:10:38 -0500

    Hello World

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import Games from "./Games/Games";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Games />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/Header/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1> Samuel & Emily Lutz Family Website</h1>;
+  }
+  
+  export default Header;
