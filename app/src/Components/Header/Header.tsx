b79c1d67a6360082b0761d68e1ae2e60ad124915
Mr-Samuel-Lutz <mr.samuel.lutz@gmail.com>
2023-12-19 14:07:11 -0600

    Added Updated Christmas Letter

--- app/src/Components/Header/Header.tsx
+++ app/src/Components/Header/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
     <div className="bg-gray-200 py-2 pl-4 rounded-sm" onClick={goTo}>
       <div className="cursor-pointer flex w-36">
         <img src={logo} alt="logo" className="w-8 mx-2" />
-        <h1 className="font-semibold">Lutz Family</h1>
+        <h1 className="font-semibold">Lutz Haus</h1>
       </div>
     </div>
   );
--- app/src/Pages/Updates/Christmas23.tsx
+++ app/src/Pages/Updates/Christmas23.tsx
@@ -7,16 +7,81 @@ const Christmas23 = () => {
         <h2 className="font-semibold text-xl mb-2">
           Merry Christmas! (December 2023)
         </h2>
-        <img src={FamilyPhoto} alt="Our Family (Sam, Emily, and Tubby)" className="rounded-xl" />
-        <div className="my-2">
-          Hello family and friends. We have had an eventful year. We adopted
-          Emily's childhood barn cat Tubby into our family. Sam has just
-          completed his second year of working for 3M as a Software Engineer. On
-          September 26th, we closed on our first house! Emily began taking
-          Realtor classes. In the meantime as she has worked on her classes,
-          Emily has also just began substitute teaching this winter.
+        <img
+          src={FamilyPhoto}
+          alt="Our Family (Sam, Emily, and Tubby)"
+          className="rounded-xl"
+        />
+        <div>
+          <div className="font-semibold text-center my-2">
+            Merry Christmas Family and Friends!
+          </div>
+          <div className="text-left indent-6">
+            We have a lot to be thankful for this past year. In February, we
+            adopted Emily's childhood barn cat Tubby into our family.
+          </div>
+          <div className="text-left indent-6">
+            In April, we entered into the Gigazone Gaming competition, where we
+            competed playing the Super Smash Bros Video Game. There were lots of
+            people, a huge stage where people competed, and during an
+            intermission, Steve Wozniak (co-founder of Apple) was there to give
+            a talk! It was a lot of fun. Sam got eliminated right away, but
+            Emily made it three rounds before being eliminated. For the event,
+            there was also a cosplay (dress up) competition, so we dressed up as
+            The Joker and Harley Quinn from Batman. We did not win that either,
+            but lots of people wanted to take pictures with us!
+          </div>
+          <div className="text-left indent-6">
+            Since we were living next door to Sam’s parents this Spring-Summer,
+            we took advantage of being on the lake. We spent a lot of time
+            reading on the lake, putt-putting around on the pontoon, and
+            enjoying nice weather with friends and family. Over the summer, Sam
+            picked up the hobby of home brewing beer. He has made four separate
+            five-gallon batches of beer this year. He has had a lot of fun
+            learning the process of brewing.
+          </div>
+          <div className="text-left indent-6">
+            On August 7th, we celebrated two years of marriage! To celebrate our
+            anniversary we went on a trip across Wisconsin. We stayed in a cute
+            little tiny cabin, got to visit friends and family, and ended the
+            trip by camping at Madeline Island, which caused us to both get
+            Bronchitis for the next couple of months. On September 26th, we
+            closed on the purchase of our first house!
+          </div>
+          <div className="my-2">
+            <div className="text-left indent-6">Our address is:</div>
+            <div className="text-left indent-12 font-semibold">1022 NE 5th Ave</div>
+            <div className="text-left indent-12 font-semibold">Grand Rapids, MN 55744</div>
+          </div>
+          <div className="text-left indent-6">
+            September 30th, we went to Brick Fest Live, a LEGO Convention in
+            Minneapolis. We had lots of fun looking at all of the LEGO builds.
+            The next day, Sam went to a Macklemore concert with his sister
+            Meghan! They had a lot of fun. A month later, Sam and Emily went
+            with Sam’s parents to a Queen + Adam Lambert concert. The rest of
+            our winter and fall has been filled in with unpacking and settling
+            into our house. When we are not doing that, we have been spending
+            time with our siblings, nieces and nephews, friends, and family. Our
+            favorite winter activities this year have been Board Games and LEGO
+            (of course). There has not been much snow yet this year, but we hope
+            to get enough to do a little skiing, and we plan to go skating this
+            break.
+          </div>
+          <div className="text-left indent-6">
+            Sam has just completed his second year of working for 3M as a
+            Software Engineer. Emily has begun taking realtor classes. As she
+            has worked on her classes, Emily has also been substitute teaching
+            this winter. Emily has been doing all of this in addition to her
+            self-owned business: MemSTAR Studio, where she converts customers’
+            home videos to digital.
+          </div>
+          <div className="text-left indent-6">
+            We hope all of our family and friends are doing well, and that you
+            all have a very Merry Christmas!
+          </div>
+          <div className="text-left indent-6">With love,</div>
+          <div className="text-left indent-12 italic">Sam & Emily Lutz</div>
         </div>
-        <div>YouTube Channel, Tubby, Camping, House, Em's Business</div>
       </div>
     </div>
   );
