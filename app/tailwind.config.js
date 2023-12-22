99a429712de5b4e7e1520df06d01d2b59a9beb79
Mr-Samuel-Lutz <mr.samuel.lutz@gmail.com>
2023-12-22 12:17:15 -0600

    Removed Address

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
+  plugins: [
+    require("tw-elements/dist/plugin"),
+    require("@material-tailwind/react")
+  ],
   theme: {
     extend: {},
   },
