65f063037cddf412131af82a9983670dea49fd94
Mr-Samuel-Lutz <mr.samuel.lutz@gmail.com>
2024-01-13 01:26:06 -0600

    Update Christmas23.tsx

--- app/src/Pages/Updates/Christmas23.tsx
+++ app/src/Pages/Updates/Christmas23.tsx
@@ -6,8 +6,8 @@ const Christmas23 = () => {
     <div>
       <div className="w-full flex justify-center">
         <div className="m-4 md:w-1/3 flex flex-col items-center">
-          <h2 className="font-semibold text-xl mb-2">
-            Merry Christmas! (December 2023)
+          <h2 className="font-semibold text-2xl mb-2 text-center">
+            Merry Christmas and a Happy New Year! (December 2023)
           </h2>
           <img
             src={FamilyPhoto}
@@ -20,7 +20,9 @@ const Christmas23 = () => {
             </div>
             <div className="text-left indent-6">
               We have a lot to be thankful for this past year. In February, we
-              adopted Emily's childhood barn cat Tubby into our family.
+              adopted Emily's childhood barn cat Tubby into our family. He is
+              now as spoiled as can be and loves being pampered in his
+              fifteen-year-old age.
             </div>
             <div className="text-left indent-6">
               In April, we entered into the Gigazone Gaming competition, where
@@ -32,12 +34,13 @@ const Christmas23 = () => {
               event, there was also a cosplay (dress up) competition, so we
               dressed up as The Joker and Harley Quinn from Batman. We did not
               win that either, but lots of people wanted to take pictures with
-              us!
+              us! We are planning on attending the same event on April 20th,
+              2024.
             </div>
             <div className="text-left indent-6">
               Since we were living next door to Sam’s parents this
               Spring-Summer, we took advantage of being on the lake. We spent a
-              lot of time reading on the lake, putt-putting around on the
+              lot of time reading on the dock, putt-putting around on the
               pontoon, and enjoying nice weather with friends and family. Over
               the summer, Sam picked up the hobby of home brewing beer. He has
               made four separate five-gallon batches of beer this year. He has
@@ -46,10 +49,12 @@ const Christmas23 = () => {
             <div className="text-left indent-6">
               On August 7th, we celebrated two years of marriage! To celebrate
               our anniversary we went on a trip across Wisconsin. We stayed in a
-              cute little tiny cabin, got to visit friends and family, and ended
-              the trip by camping at Madeline Island, which caused us to both
-              get Bronchitis for the next couple of months. On September 26th,
-              we closed on the purchase of our first house!
+              cute little tiny cabin, attended a friend's wedding, got to visit
+              friends and family, and ended the trip by camping at Madeline
+              Island, which caused us both to get Bronchitis for the next couple
+              of months. On September 26th, we closed on the purchase of our
+              first house! We are living in Grand Rapids, MN. Feel free to text
+              us for our address.
             </div>
             <div className="text-left indent-6">
               September 30th, we went to Brick Fest Live, a LEGO Convention in
@@ -59,11 +64,26 @@ const Christmas23 = () => {
               with Sam’s parents to a Queen + Adam Lambert concert. The rest of
               our winter and fall has been filled in with unpacking and settling
               into our house. When we are not doing that, we have been spending
-              time with our siblings, nieces and nephews, friends, and family.
-              Our favorite winter activities this year have been Board Games and
-              LEGO (of course). There has not been much snow yet this year, but
-              we hope to get enough to do a little skiing, and we plan to go
-              skating this break.
+              time with our siblings, nieces, nephews, friends, and family. Our
+              favorite winter activities this year have been Board Games and
+              LEGO (of course). Emily started a YouTube Channel earlier this
+              year and likes to to make videos about LEGO in her free time. If
+              you are interested in showing your support and keeping up to date
+              on our LEGO-building activities, you can find the&nbsp;
+              <div className="italic inline-block indent-0">
+                Average Brick Chick
+              </div>
+              &nbsp;YouTube Channel&nbsp;
+              <a
+                className="cursor-pointer text-blue-600 font-semibold hover:underline"
+                rel="noreferrer noopener"
+                target="_blank"
+                href="https://www.youtube.com/@AverageBrickChick"
+              >
+                here
+              </a>
+              . There has not been much snow yet this year, but we hope to get
+              enough to do a little skiing and skating in the new year.
             </div>
             <div className="text-left indent-6">
               Sam has just completed his second year of working for 3M as a
