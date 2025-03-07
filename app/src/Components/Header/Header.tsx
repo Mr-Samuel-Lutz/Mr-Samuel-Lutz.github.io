d7ae7b9bd578bb75ca893304e3026463551698f4
Mr-Samuel-Lutz <mr.samuel.lutz@gmail.com>
2025-03-06 20:22:49 -0600

    Remove All Routing Including Header

--- app/src/Components/Header/Header.tsx
+++ app/src/Components/Header/Header.tsx
@@ -1,15 +1,8 @@
-import { useNavigate } from "react-router-dom";
 import logo from "../../Images/brick.png";
 
 function Header() {
-  const navigate = useNavigate();
-
-  const goTo = () => {
-    navigate("/");
-  };
-
   return (
-    <div className="bg-gray-200 py-2 pl-4 rounded-sm" onClick={goTo}>
+    <div className="bg-gray-200 py-2 pl-4 rounded-sm">
       <div className="cursor-pointer flex w-36">
         <img src={logo} alt="logo" className="w-8 mx-2" />
         <h1 className="font-semibold">Lutz Haus</h1>
